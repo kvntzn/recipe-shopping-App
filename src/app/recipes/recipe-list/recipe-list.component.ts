9c5913349be64075cf2cd682179e85d613d75830
Kevin Tuazon <kevin.tuazon@accenture.com>
2021-11-30 00:14:49 +0800

    section 3 finished

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Recipe } from "../recipe.model";
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      "test reciper",
+      "this is a test",
+      "https://static.onecms.io/wp-content/uploads/sites/43/2020/07/22/8000900-2000.jpg"
+    ),
+    new Recipe(
+      "test reciper",
+      "this is a test",
+      "https://static.onecms.io/wp-content/uploads/sites/43/2020/07/22/8000900-2000.jpg"
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
