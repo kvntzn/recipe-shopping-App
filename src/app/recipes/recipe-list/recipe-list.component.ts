82e692e7420837dcc0ab4803400b79714af70791
Kevin Tuazon <kevin.tuazon@accenture.com>
2021-12-06 23:33:52 +0800

    using services

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from "@angular/core";
 import { Recipe } from "../recipe.model";
+import { RecipeService } from "../recipe.service";
 
 @Component({
   selector: "app-recipe-list",
@@ -9,22 +10,13 @@ import { Recipe } from "../recipe.model";
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
-    new Recipe(
-      "test reciper",
-      "this is a test",
-      "https://static.onecms.io/wp-content/uploads/sites/43/2020/07/22/8000900-2000.jpg"
-    ),
-    new Recipe(
-      "test reciper 2",
-      "this is a test",
-      "https://static.onecms.io/wp-content/uploads/sites/43/2020/07/22/8000900-2000.jpg"
-    ),
-  ];
+  recipes: Recipe[] = [];
 
-  constructor() {}
+  constructor(private recipeService: RecipeService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
+  }
 
   onRecipeSelected(recipe: Recipe) {
     this.recipeWasSelected.emit(recipe);
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { Recipe } from "./recipe.model";
+
+// @Injectable({ providedIn: "root" })
+export class RecipeService {
+  constructor() {}
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      "test reciper",
+      "this is a test",
+      "https://static.onecms.io/wp-content/uploads/sites/43/2020/07/22/8000900-2000.jpg"
+    ),
+    new Recipe(
+      "test reciper 2",
+      "this is a test",
+      "https://static.onecms.io/wp-content/uploads/sites/43/2020/07/22/8000900-2000.jpg"
+    ),
+  ];
+
+  getRecipes(): Recipe[] {
+    return this.recipes.slice();
+  }
+}
