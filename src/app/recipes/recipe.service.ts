dc642cad6486d848c03675ba33abc91be113a177
Kevin Tuazon <kevintuazon45@gmail.com>
2021-12-07 14:38:36 +0800

    updated service adding ingredients in recipe modle

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 // @Injectable({ providedIn: "root" })
@@ -10,12 +11,14 @@ export class RecipeService {
     new Recipe(
       "test reciper",
       "this is a test",
-      "https://static.onecms.io/wp-content/uploads/sites/43/2020/07/22/8000900-2000.jpg"
+      "https://static.onecms.io/wp-content/uploads/sites/43/2020/07/22/8000900-2000.jpg",
+      [new Ingredient("Meat", 1), new Ingredient("French Fries", 1)]
     ),
     new Recipe(
       "test reciper 2",
       "this is a test",
-      "https://static.onecms.io/wp-content/uploads/sites/43/2020/07/22/8000900-2000.jpg"
+      "https://static.onecms.io/wp-content/uploads/sites/43/2020/07/22/8000900-2000.jpg",
+      [new Ingredient("Meat", 1), new Ingredient("French Fries", 1)]
     ),
   ];
 
