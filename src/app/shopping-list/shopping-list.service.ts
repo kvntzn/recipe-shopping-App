3f4e0f3e06a07d2774c753c5436fe168305b9036
Kevin Tuazon <kevintuazon45@gmail.com>
2021-12-07 14:30:10 +0800

    updated shopping list service with event emitter

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,6 +1,23 @@
-import { Injectable } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
-@Injectable({providedIn: 'root'})
-export class ServiceNameService {
+@Injectable({ providedIn: "root" })
+export class ShoppingListService {
   constructor() {}
+
+  ingredientsChange = new EventEmitter<Ingredient[]>();
+
+  private ingredients: Ingredient[] = [
+    new Ingredient("apples", 5),
+    new Ingredient("tomato", 5),
+  ];
+
+  getIngredients(): Ingredient[] {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChange.emit(this.ingredients.slice());
+  }
 }
