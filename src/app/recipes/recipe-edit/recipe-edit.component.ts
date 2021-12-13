b3f7f443dc06dcebf61ee777ca462d42a351938a
Kevin Tuazon <kevintuazon45@gmail.com>
2021-12-13 15:16:46 +0800

    updated routing

---
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-recipe-edit',
+  templateUrl: './recipe-edit.component.html',
+  styleUrls: ['./recipe-edit.component.css']
+})
+export class RecipeEditComponent implements OnInit {
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+  }
+
+}
