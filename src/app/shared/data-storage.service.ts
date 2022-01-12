f66332afed95d321cfe195bcc9bc203c99c2076f
Kevin Tuazon <kevintuazon45@gmail.com>
2022-01-12 23:00:22 +0800

    resolver fetch

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
+import { map, tap } from "rxjs/operators";
 
 @Injectable({ providedIn: "root" })
 export class DataStorageService {
@@ -21,10 +22,18 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http.get<Recipe[]>(this.RECIPE_URL).subscribe((recipes) => {
-      console.log(recipes);
-      this.recipesService.setRecipes(recipes);
-    });
-    // this.recipesService.
+    return this.http.get<Recipe[]>(this.RECIPE_URL).pipe(
+      map((recipes) => {
+        return recipes.map((recipe) => {
+          return {
+            ...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : [],
+          };
+        });
+      }),
+      tap((recipes) => {
+        this.recipesService.setRecipes(recipes);
+      })
+    );
   }
 }
