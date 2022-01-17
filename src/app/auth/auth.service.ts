d4f33202fd4bdebdd6a65a1593c955a8f42bc831
Kevin Tuazon <kevintuazon45@gmail.com>
2022-01-17 14:39:45 +0800

    updatted signup

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
 
 interface AuthResponseData {
   idToken: string;
@@ -14,13 +16,33 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   signup(email: string, password: string) {
-    return this.http.post<AuthResponseData>(
-      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAPznpQcwXGeYSYYWC-Iv6EMWAom69tQaU",
-      {
-        email: email,
-        password: password,
-        returnSecureToken: true,
-      }
-    );
+    return this.http
+      .post<AuthResponseData>(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAPznpQcwXGeYSYYWC-Iv6EMWAom69tQaU",
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(
+        catchError((errorRes) => {
+          let errorMessage = "An unkown error occurred!";
+
+          if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage);
+          }
+
+          switch (errorRes.error.error.message) {
+            case "EMAIL_EXISTS":
+              errorMessage = "This email exists already";
+              break;
+            default:
+              errorMessage = "An error occurred";
+          }
+
+          return throwError(errorMessage);
+        })
+      );
   }
 }
