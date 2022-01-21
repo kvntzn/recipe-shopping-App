7110129e4ce39b2f8030f5ffab7a63c7d4000db2
Kevin Tuazon <kevintuazon45@gmail.com>
2022-01-21 14:35:04 +0800

    core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,15 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { AppRoutingModule } from "./app-routing.module";
-import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { HttpClientModule } from "@angular/common/http";
 import { AuthComponent } from "./auth/auth.component";
-import { AuthInterceptorService } from "./auth/auth-interceptor.service";
 import { RecipesModule } from "./recipes/recipes.module";
 import { ShoppingListModule } from "./shopping-list/shopping-list.module";
 import { SharedModule } from "./shared/shared.module";
+import { CoreModule } from "./core.module";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    AuthComponent,
-  ],
+  declarations: [AppComponent, HeaderComponent, AuthComponent],
   imports: [
     BrowserModule,
     FormsModule,
@@ -26,14 +22,8 @@ import { SharedModule } from "./shared/shared.module";
     ReactiveFormsModule,
     RecipesModule,
     ShoppingListModule,
-    SharedModule
-  ],
-  providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptorService,
-      multi: true,
-    },
+    SharedModule,
+    CoreModule,
   ],
   bootstrap: [AppComponent],
 })
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,7 +8,20 @@ import { Recipe } from "./recipe.model";
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = []
+  private recipes: Recipe[] = [
+    new Recipe(
+      "test reciper",
+      "this is a test",
+      "https://static.onecms.io/wp-content/uploads/sites/43/2020/07/22/8000900-2000.jpg",
+      [new Ingredient("Meat", 1), new Ingredient("French Fries", 1)]
+    ),
+    new Recipe(
+      "test reciper 2",
+      "this is a test",
+      "https://static.onecms.io/wp-content/uploads/sites/43/2020/07/22/8000900-2000.jpg",
+      [new Ingredient("Meat", 1), new Ingredient("French Fries", 1)]
+    ),
+  ];
 
   constructor(private slService: ShoppingListService) {}
 
