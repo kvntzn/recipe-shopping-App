f6699e8ec3b0d9b79c2645ac15eb9a7a5ed1fc97
Kevin Tuazon <kevintuazon45@gmail.com>
2022-01-21 15:52:46 +0800

    routing load lazy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,23 @@ import { AuthComponent } from "./auth/auth.component";
 
 const appRoutes: Routes = [
   { path: "", redirectTo: "/recipes", pathMatch: "full" },
+  {
+    path: "auth",
+    loadChildren: () => import("./auth/auth.module").then((m) => m.AuthModule),
+  },
+  {
+    path: "recipes",
+    // loadChildren: "./recipes/recipes.module#RecipesModule",
+    loadChildren: () =>
+      import("./recipes/recipes.module").then((m) => m.RecipesModule),
+  },
+  {
+    path: "shopping-list",
+    loadChildren: () =>
+      import("./shopping-list/shopping-list.module").then(
+        (m) => m.ShoppingListModule
+      ),
+  },
 ];
 
 @NgModule({
